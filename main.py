b38fcef2dd0a2a7158834fd81eaecd8bb3d883c6
PremPremPremPrem <95293374+PremPremPremPrem@users.noreply.github.com>
2023-03-07 15:17:21 +0700

    Adding basic prompts

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from pyChatGPT import ChatGPT
 from creds import session_token
+from random import randint
 
 api = ChatGPT(session_token)
 
-resp = api.send_message('Hello, world!')
+resp = api.send_message(f'Can you give me 10 ideas for a {randint(1, 3)}-minute coding video?')
+get_ideas = resp['message']
 print(resp['message'])
-api.clear_conversations()
+
+resp = api.send_message(f'Can I see a script of #{randint(1, 10)}; Code snippets included, please.')
+get_script = resp['message']
+print(resp['message'])
