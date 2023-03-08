d7310a778a44848160c4a86a5d6dac8a68845b41
PremPremPremPrem <95293374+PremPremPremPrem@users.noreply.github.com>
2023-03-08 15:32:00 +0700

    gTTS accidentally removed, with SpeechBrain. Reverted.

--- main.py
+++ main.py
@@ -21,4 +21,8 @@ except FileExistsError:
 # for i in gtts_filter:
 #     remove_from_gtts = not remove_from_gtts if i == "♦" else remove_from_gtts
 #     gtts_speech += i if not remove_from_gtts else ''
+
+tts = gTTS(get_script)
+tts.save('resources/tts.mp3')
+
 print(get_script)
