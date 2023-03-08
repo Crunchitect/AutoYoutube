27a9df10c1b1446af76af55e425833c2a011081d
PremPremPremPrem <95293374+PremPremPremPrem@users.noreply.github.com>
2023-03-08 15:04:50 +0700

    Changed current API backend from pyChatGPT to hugging face.

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from gtts import gTTS
+from transformers import pipeline, set_seed
 from os import mkdir
 
+generator = pipeline('text-generation', model='gpt2-large')
+set_seed(42)
+get_script = generator('Hello Everyone, And welcome to another coding tutorial! Today we will talk about',
+                       max_length=150)
 
-get_script = "\"wow\"so hello\"but\"nevermind"
+get_script = get_script[0]["generated_text"]
 
 try:
     mkdir('resources')
 except FileExistsError:
     pass
 
-gtts_filter = str(get_script)
-gtts_filter = gtts_filter.replace("\"", "♦")
-gtts_speech = ''
-remove_from_gtts = False
-for i in gtts_filter:
-    remove_from_gtts = not remove_from_gtts if i == "♦" else remove_from_gtts
-    gtts_speech += i if not remove_from_gtts else ''
-print(gtts_speech)
-tts = gTTS(gtts_speech)
+# gtts_filter = str(get_script)
+# gtts_filter = gtts_filter.replace("\"", "♦")
+# gtts_speech = ''
+# remove_from_gtts = False
+# for i in gtts_filter:
+#     remove_from_gtts = not remove_from_gtts if i == "♦" else remove_from_gtts
+#     gtts_speech += i if not remove_from_gtts else ''
+print(get_script)
+tts = gTTS(get_script)
 tts.save('resources/tts.mp3')
