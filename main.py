c01cdc1f267985f179ad098e0ba2c36256cc214e
PremPremPremPrem <95293374+PremPremPremPrem@users.noreply.github.com>
2023-03-10 12:57:48 +0700

    Solving sudoku

--- main.py
+++ main.py
@@ -93,6 +93,34 @@ def main(*, verbose=False):
     if verbose:
         print("\u001b[36m ⓘ Defining Functions")
 
+    class SudokuSolver:
+        @staticmethod
+        def is_possible(grid, y, x, n):
+            for i in range(0, 9):
+                if grid[y][i] == n:
+                    return False
+            for i in range(0, 9):
+                if grid[i][x] == n:
+                    return False
+            x0 = (x//3)*3
+            y0 = (y//3)*3
+            for i in range(0, 3):
+                for j in range(0, 3):
+                    if grid[y0+i][x0+j] == n:
+                        return False
+            return True
+
+        @staticmethod
+        def solve(g):
+            for y in range(9):
+                for x in range(9):
+                    if g[y][x] == 0:
+                        for n in range(1, 10):
+                            if SudokuSolver.is_possible(g, y, x, n):
+                                g[y][x] = n
+                                return y, x
+                        return 0 / 0
+
     def create_files():
         try:
             mkdir('resources')
@@ -101,7 +129,7 @@ def main(*, verbose=False):
 
         try:
             mkdir('resources/frames')
-            files = glob('/YOUR/PATH/*')
+            files = glob('resources/frames/*')
             for f in files:
                 remove(f)
         except FileExistsError:
@@ -132,24 +160,34 @@ def main(*, verbose=False):
 
         return board
 
-    def generate_frame(board):
+    def generate_frame(b):
         # import image
         w = 1080
         h = 1920
         sudoku_frame = Image.new("RGB", (w, h), color=(255, 255, 255))
         ImPen.set_stroke(5)
-        ImPen.impen_grid(sudoku_frame, 9, 9, 100, 100)
+        # ImPen.impen_grid(sudoku_frame, 9, 9, 100, 100)
         ImPen.impen_text(sudoku_frame, 0, 0, "Easy Sudoku")
         for x in range(0, 9):
             for y in range(0, 9):
-                ImPen.impen_text_grid(sudoku_frame, 9, 9, 100, 100, x, y, board[y][x] if board[y][x] != 0 else " ")
+                ImPen.impen_text_grid(sudoku_frame, 9, 9, 100, 100, x, y, b[y][x] if b[y][x] != 0 else " ")
         return sudoku_frame
 
     print("\u001b[36m ⓘ Creating Files" if verbose else "")
     create_files()
     print("\u001b[36m ⓘ Generating Sudoku" if verbose else "")
     print("\u001b[36m ⓘ Creating Frames" if verbose else "")
-    generate_frame(generate_sudoku()).save("resources/frames/1.png")
+    frame_no = 1
+    grid = generate_sudoku()
+    prev_grid = generate_sudoku()
+    while any(0 in sublist for sublist in grid):
+        generate_frame(grid).save(f"resources/frames/{frame_no}.png")
+        iy, ix = SudokuSolver.solve(grid)
+        if prev_grid == grid:
+            grid[iy][ix] = 0
+        print(f"\t\u001b[36m ⓘ Creating Frame #{frame_no}" if verbose else "")
+        frame_no += 1
+        prev_grid = grid
 
 
 if __name__ == '__main__':
